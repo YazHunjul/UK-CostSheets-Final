3e7124a2aa618de0c1aeecf03af929229126dd4a
Yazan <178886158+YazHunjul@users.noreply.github.com>
2025-03-18 11:10:45 -0400

    q

--- app/costSheetGen/Canopy/canopyExcel.py
+++ app/costSheetGen/Canopy/canopyExcel.py
@@ -41,7 +41,7 @@ def generate_sheet(kitchen_info, genInfo):
     """
     try:
         # Load the workbook
-        excel_path = '/Users/yazan/Desktop/Efficiency/UK-CostSheets-Final/app/costSheetGen/costSheetResources/Halton Cost Sheet Jan 2025.xlsx'
+        excel_path = 'app/costSheetGen/costSheetResources/Halton Cost Sheet Jan 2025.xlsx'
         wb = load_workbook(excel_path, data_only=False)  # Keep formulas
         
         # Keep a clean template
